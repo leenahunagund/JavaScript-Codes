d01ee0d3375b8f99959018bb78e0bd6e1b2556a5
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-02-24 01:03:26 +0530

    Add files via upload

---
+++ scripts/calc.js
@@ -0,0 +1,37 @@
+function calculate(number){
+    let calculation='';
+    if(number==='1')
+    console.log(calculation+='1');
+    else if (number==='2')
+    console.log(calculation+='2');
+    else if (number==='3')
+    console.log(calculation+='3');
+    else if (number==='4')
+    console.log(calculation+='4');
+    else if (number==='5')
+    console.log(calculation+='5');
+    else if (number==='6')
+    console.log(calculation+='6');
+    else if (number==='7')
+    console.log(calculation+='7');
+    else if (number==='8')
+    console.log(calculation+='8');
+    else if (number==='9')
+    console.log(calculation+='9');
+    else if (number==='0')
+    console.log(calculation+='0');
+    else if(number==='+')
+    console.log(calculation+='+');
+    else if(number==='-')
+    console.log(calculation+='-');
+    else if(number==='*')
+    console.log(calculation+='*');
+    else if(number==='/')
+    console.log(calculation+='/');
+    else if (number==='.')
+    console.log(calculation+='.');
+    else if (number==='=')
+    console.log(eval(calculation));
+    else if (number==='Clear')
+    console.log(' ');
+}
